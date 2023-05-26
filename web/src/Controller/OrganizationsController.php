b7a397c50287259a13500d28542726db1fa858cd
Bishwajit Das <bishwajitkuat@gmail.com>
2023-05-27 01:35:44 +0600

    getAllOrganizations get api to get all organizations is created

--- web/src/Controller/OrganizationsController.php
+++ web/src/Controller/OrganizationsController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Organizations;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrganizationsController extends AbstractController
 {
-    #[Route('/organizations', name: 'app_organizations')]
-    public function index(): JsonResponse
+    #[Route('/getAllOrganizations', name:'getAllOrganizations', methods:['GET'])]
+function getAllOrganizations(ManagerRegistry $doctrine): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/OrganizationsController.php',
+    $em = $doctrine->getManager();
+    $organizations = $em->getRepository(Organizations::class)->findAll();
+    $data = [];
+    foreach ($organizations as $org) {
+        array_push($data, [
+            "id" => $org->getId(),
+            "name" => $org->getName(),
+            "image" => $org->getImage(),
+            "locatiion" => $org->getLocatiion(),
+            "description" => $org->getDescription(),
+            "email" => $org->getEmail(),
+            "phone" => $org->getPhone(),
+            "created_at" => $org->getCreatedAt(),
+            "updated_at" => $org->getUpdatedAt(),
+
         ]);
     }
+    return $this->json($data);
+}
 }
